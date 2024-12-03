d607bc209779f8cdccfbe453fdb74d77ea8dd859
KOtiyaReba <knowledge.officer3@ictkerala.org>
2024-12-03 10:10:40 +0530

    dist file added

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ app.put("/edit/:id", async (req, res) => {
   }
 });
 
-app.use("/", express.static(path.join(__dirname, "./build")));
+app.use("/", express.static(path.join(__dirname, "./dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
+  res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
 
